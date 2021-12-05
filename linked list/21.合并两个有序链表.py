43a3cb5b57a151762e1857ad7bc4a1e12d519195
candicej <34505774+candicej@users.noreply.github.com>
2021-12-05 19:11:33 +0800

    Update 21.合并两个有序链表.py

--- linked list/21.合并两个有序链表.py
+++ linked list/21.合并两个有序链表.py
@@ -4,6 +4,8 @@
 #         self.val = val
 #         self.next = next
 # 方法一： 递归
+# 这个题解里的图有助于理解
+# https://leetcode-cn.com/problems/merge-two-sorted-lists/solution/yi-kan-jiu-hui-yi-xie-jiu-fei-xiang-jie-di-gui-by-/
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         # 递归停止的条件 边界条件
@@ -14,8 +16,10 @@ class Solution:
         
         # 递归 需要叠加栈
         if l1.val <= l2.val:
+            # 把问题向下求解 
             l1.next = self.mergeTwoLists(l1.next, l2)
-            # 这个地方不太理解
+            # 这个地方很关键
+            # 将结果返回 
             return l1
         
         if l1.val > l2.val:
